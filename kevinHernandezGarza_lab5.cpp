520fb58ce4d4aac936a8fa8a68b03ab3d0ca07a7
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-03-31 11:35:34 -0500

    updated comments

--- kevinHernandezGarza_lab5.cpp
+++ kevinHernandezGarza_lab5.cpp
@@ -1,11 +1,5 @@
 //******************************************************************
 // Author: Kevin Hernandez-Garza
-// Assignment Number: Programming Assignment "5"
-// File Name: KevinHernandezGarza_lab5.cpp
-// Course/Section: COSC 1337 Section -009
-// Due Date: 3/25/23
-// Instructor: Dr.Haytham Mohamed
-//
 // This program tracks information on water consumption for the city of Paris,
 // Texas on a monthly basis. It specifically calculates the highest and lowest
 // months, and yearly average and total measured in CCF(centum cubic feet).
